36f57a41c69b507da590da2ca7a4313f3a65345b
Stephan Mannhart <stephan.mannhart@googlemail.com>
2015-09-11 15:28:02 +0200

    Added functionality to remove old dotfiles first.

--- symlink.sh
+++ symlink.sh
@@ -9,6 +9,11 @@ cd $dir
 echo "done"
 
 for file in $files; do
+	echo "Removing existing dotfiles from ~"
+	rm -rf ~/.$file
+done
+
+for file in $files; do
     echo "Creating symlink to $file in home directory"
     ln -s $dir/$file ~/.$file
 done
