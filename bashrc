d900569172c2b8ca295744d6127da3905ca3b4cf
Stephan Mannhart <stephan.mannhart@googlemail.com>
2015-10-02 14:50:56 +0200

    Aliases are now created with the symlink script

--- bashrc
+++ bashrc
@@ -2,6 +2,11 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
+# add system-specific aliases
+if [ -f "$HOME/.aliases" ]; then
+. "$HOME/.aliases"
+fi
+
 # If not running interactively, don't do anything
 case $- in
     *i*) ;;
--- symlink.sh
+++ symlink.sh
@@ -2,7 +2,7 @@
 # Inspired by https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
 
 dir=~/dotfiles
-files="profile bashrc vimrc vim bin tmux.conf lessfilter aliases"
+files="profile bashrc vimrc vim bin tmux.conf lessfilter"
 
 echo -n "Changing to the $dir directory for correct symlinks ... "
 cd $dir
@@ -12,6 +12,13 @@ for file in $files; do
 	echo "Removing existing dotfiles from ~"
 	rm -rf ~/.$file
 done
+
+if [ ! -f ~/.aliases ]
+then
+	echo "Creating empty .aliases file and symlink"
+	touch ~/dotfiles/aliases
+	ln -s ~/dotfiles/aliases ~/.aliases
+fi
 
 for file in $files; do
     echo "Creating symlink to $file in home directory"
